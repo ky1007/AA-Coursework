f77d6037b92ca5a7ab16c8ad20122cbd6e9ec66d
Kevin Yang <kevinXyang@outlook.com>
2017-06-14 11:32:19 -0700

    Actually complete enrollment associations, change student_id

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,14 +1,14 @@
 class Enrollment < ActiveRecord::Base
 
-belongs_to :course,
-  primary_key: :id,
-  foreign_key: :course_id,
-  class_name: :Course
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
 
-belongs_to :user,
-  primary_key: :id,
-  foreign_key: :user_id,
-  class_name: :User
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
 
 
 end
