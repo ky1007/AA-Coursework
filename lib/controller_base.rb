4b90dfc41de7a3b2ffded410398b5b25ca1e077f
Kevin Yang <kevinXyang@outlook.com>
2017-06-27 14:35:18 -0700

    Finish methods outlined in the controller spec

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,20 +8,35 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "You've already responded to this!" if already_built_response?
+    
+    @res.header['location'] = url
+    @res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "You've already rendered this!" if already_built_response?
+
+    unless already_built_response?
+      @res['Content-Type'] = content_type
+      @res.write("#{content}")
+      @already_built_response = true
+    end
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +52,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
